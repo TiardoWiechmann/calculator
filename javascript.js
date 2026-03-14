96d6cdc2166b0b91df445e244728db9ee939a610
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-03-14 13:05:04 +0100

    Set up files and create basic math functions

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// Calculator should be able to perform +, -, *, /
+
+// Create functions (add, subtract, multiply, divide)
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b; 
+}
+
+// Create function operate, that takes an operator and two numbers and performs one of the above functions
+
