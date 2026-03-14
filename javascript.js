839221a7a82b71579bffbe9a36c4e9feee5babae
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-03-14 13:14:18 +0100

    Implement operate to select basic math function

--- javascript.js
+++ javascript.js
@@ -18,4 +18,29 @@ function divide(a, b) {
 }
 
 // Create function operate, that takes an operator and two numbers and performs one of the above functions
+function operate(num1, op, num2) {
+    if (op === "+") {
+        return add(num1, num2);
+    }
+    else if (op === "-") {
+        return subtract(num1, num2);
+    }
+    else if (op === "*") {
+        return multiply(num1, num2);
+    }
+    else if (op === "/") {
+        return divide(num1, num2);
+    }
+}
 
+
+
+// const a = operate(1, "+", 2);
+// const b = operate(1, "-", 2);
+// const c = operate(1, "*", 2);
+// const d = operate(1, "/", 2);
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
