9dc5a5b6e901cfba82b70d7f89ed54a9ca77a299
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-03-15 08:19:31 +0100

    Finish basic logic of calculator

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 // Calculator should be able to perform +, -, *, /
 
-// Create functions (add, subtract, multiply, divide)
+
 function add(a, b) {
     return a + b;
 }
@@ -17,31 +17,113 @@ function divide(a, b) {
     return a / b; 
 }
 
-// Create function operate, that takes an operator and two numbers and performs one of the above functions
+
 function operate(num1, op, num2) {
+    let result = 0;
     if (op === "+") {
-        return add(num1, num2);
+        result =  add(num1, num2);
     }
     else if (op === "-") {
-        return subtract(num1, num2);
+        result =  subtract(num1, num2);
     }
-    else if (op === "*") {
-        return multiply(num1, num2);
+    else if (op === "x") {
+        result = multiply(num1, num2);
     }
     else if (op === "/") {
-        return divide(num1, num2);
+        result = divide(num1, num2);
     }
+    return result = roundPlaces(result, 6);
+}
+
+function roundPlaces(n, places) {
+    return Math.round(n * (10**places)) / (10**places);
 }
 
 
+function updateNum(num, digit) {
+    num += digit;
+    return num;
+}
 
 
-// const a = operate(1, "+", 2);
-// const b = operate(1, "-", 2);
-// const c = operate(1, "*", 2);
-// const d = operate(1, "/", 2);
+function updateDisplay(num1, op, num2){
+    const display = document.querySelector(".display");
+    if (op === undefined && num2 === undefined) {
+        display.textContent = num1;
+    }
+    else if (num2 === undefined) {
+        display.textContent = `${num1} ${op}`;
+    }
+    else {
+        display.textContent = `${num1} ${op} ${num2}`;
+    }
+}
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
+function zeroDivision(op, num2) {
+    if (op === "/" && num2 === "0") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const operators = "+-x/";
+const reNum = /[0-9]+/;
+let num1 = "";
+let num2 = "";
+let op = "";
+let resIsDisplayed = false;
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (button.parentElement.className == "row-digit"){
+            const digit = button.textContent;
+            if (op === "") {
+                if (resIsDisplayed){
+                    num1 = "";
+                }
+                num1 = updateNum(num1, digit);
+                updateDisplay(num1);
+            }
+            else {
+                num2 = updateNum(num2, digit);
+                updateDisplay(num1, op, num2);
+            }
+        }
+        else if (button.className == "op") {
+            if (reNum.test(num1) && operators.includes(op) && reNum.test(num2) && !zeroDivision(op, num2)) {
+                num1 = operate(+num1, op, +num2);
+                num2 = "";
+            }
+            op = button.textContent;
+            updateDisplay(num1, op);
+        }
+        else if (button.className == "result") {
+            // All components exist
+            if (reNum.test(num1) && operators.includes(op) && reNum.test(num2) && !zeroDivision(op, num2)) {
+                num1 = operate(+num1, op, +num2);
+            }
+            // Only num1 exists
+            else if (reNum.test(num1) &&  !zeroDivision(op, num2)){
+                 num1 = num1;
+             }
+            // Not a valid expression
+            else {
+                num1 = "";
+            }
+            updateDisplay(num1);
+            resIsDisplayed = true;
+            op = "";
+            num2 = "";
+        }
+        else if (button.textContent == "AC") {
+            num1 = "";
+            num2 = "";
+            op = "";
+            updateDisplay("");
+        }
+    })
+})
+
